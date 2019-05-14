f9d0d64b37697f7513d744c4e9d9798466e9c034
Rees Dooley <rees@blacklocus.com>
2019-05-14 11:57:41 -0500

    Write command: now working
    * reading and writing!
    * TODO scanning table
    * TODO is_protected support
    * TODO diff support

--- main.go
+++ main.go
@@ -124,7 +124,45 @@ func Write(table string, path string) {
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-	// now write to dynamo TODO
+	svc := dynamodb.New(session.New(&aws.Config{Region: aws.String("us-east-1")}))
+	var item = map[string]*dynamodb.AttributeValue{}
+	for k, v := range config {
+		item[k] = &dynamodb.AttributeValue{S: aws.String(v)}
+	}
+	input := &dynamodb.PutItemInput{
+		Item:      item,
+		TableName: aws.String(table),
+	}
+
+	_, err = svc.PutItem(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case dynamodb.ErrCodeConditionalCheckFailedException:
+				fmt.Println(dynamodb.ErrCodeConditionalCheckFailedException, aerr.Error())
+			case dynamodb.ErrCodeProvisionedThroughputExceededException:
+				fmt.Println(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
+			case dynamodb.ErrCodeResourceNotFoundException:
+				fmt.Println(dynamodb.ErrCodeResourceNotFoundException, aerr.Error())
+			case dynamodb.ErrCodeItemCollectionSizeLimitExceededException:
+				fmt.Println(dynamodb.ErrCodeItemCollectionSizeLimitExceededException, aerr.Error())
+			case dynamodb.ErrCodeTransactionConflictException:
+				fmt.Println(dynamodb.ErrCodeTransactionConflictException, aerr.Error())
+			case dynamodb.ErrCodeRequestLimitExceeded:
+				fmt.Println(dynamodb.ErrCodeRequestLimitExceeded, aerr.Error())
+			case dynamodb.ErrCodeInternalServerError:
+				fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+	fmt.Printf("%s written to %s\n", path, table)
 }
 
 func main() {
